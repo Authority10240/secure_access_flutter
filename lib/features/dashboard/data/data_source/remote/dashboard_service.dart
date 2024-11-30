557396c0eee1592e5ccd99796ee0e0227cbc1692
Authority10240 <themba10240@gmail.com>
2024-11-30 06:56:48 +0200

    added timestamp for queries to objects

--- lib/features/dashboard/data/data_source/remote/dashboard_service.dart
+++ lib/features/dashboard/data/data_source/remote/dashboard_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:secure_access/features/dashboard/data/models/dashboard_model_response/dashboard_page_load_visitations_vehicle_model.dart';
 import 'package:secure_access/features/dashboard/data/models/dashboard_model_response/dashboard_page_load_vistations_model.dart';
-import 'package:secure_access/features/dashboard/presentation/bloc/dashboard_bloc.dart';
+
 
 abstract class DashboardService {
 
